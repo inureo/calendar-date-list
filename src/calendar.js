ff3b3da3984e70dc7652e0b4d181ac26b44805c5
Ryo Tezuka <tezuka0226@gmail.com>
2019-03-31 18:27:40 +0900

    [add] calendar.js (#1)
    * [add] calendar.js
    
    * add: build status in README.md

---
+++ src/calendar.js
@@ -0,0 +1,43 @@
+import {
+  getLastDate,
+  createDateArray,
+  reindexDayOfWeek
+} from './util'
+
+export default class Calendar {
+  constructor(firstWeekDay = 0) {
+    if (typeof firstWeekDay !== 'number') {
+      throw new Error('firstWeekDay must be a number')
+    }
+    this.DAY_OF_WEEK_INDEX = 6
+    this.firstWeekDay = firstWeekDay
+    this.reindexDayOfWeek = reindexDayOfWeek(this.firstWeekDay)
+  }
+
+  getDates(year, month) {
+    const firstDate = new Date(year, month, 1)
+    const lastDate = getLastDate(firstDate)
+    const lastMonthDate = new Date(year, month, 0)
+    const nextMonthDate = new Date(year, month + 1, 1)
+
+    const lastMonthShortageDays = this.reindexDayOfWeek[firstDate.getDay()]
+    const lastMonthShortageDates = createDateArray(
+        lastMonthDate,
+        lastMonthShortageDays,
+        lastMonthDate.getDate() - lastMonthShortageDays + 1
+    )
+    const nextMonthShortageDates = createDateArray(nextMonthDate, this._compensateNextMonthDates(lastDate.getDay()))
+    const currentMonthDates = createDateArray(lastDate, lastDate.getDate())
+
+    const dates = [...lastMonthShortageDates, ...currentMonthDates, ...nextMonthShortageDates]
+
+    return dates.reduce((rows, key, index) => {
+      index % 7 === 0 ? rows.push([key]) : rows[rows.length - 1].push(key)
+      return rows
+    }, [])
+  }
+
+  _compensateNextMonthDates(index) {
+    return this.DAY_OF_WEEK_INDEX - this.reindexDayOfWeek[index]
+  }
+}
