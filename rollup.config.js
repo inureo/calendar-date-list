64d3ac0fef5300a5deb699a5561733daa94cf8a8
inureo <tezuka0226@gmail.com>
2019-05-29 21:44:29 +0900

    add: node-resolver

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,25 @@
-import babel from 'rollup-plugin-babel';
-import {uglify} from "rollup-plugin-uglify";
-import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import {uglify} from "rollup-plugin-uglify"
+import pkg from './package.json'
 
-export default [
-  {
-    input: 'src/calendar-date-list.js',
-    output: {
-      name: 'calendar-date-list',
-      file: 'dist/calendar-date-list.js',
-      format: 'umd',
-      exports: 'default',
-      banner: `/* ${pkg.name} v${pkg.version}, @license MIT */`
-    },
-    plugins: [
-      babel({
-        runtimeHelpers: true
-      }),
-      uglify({
-        output: {
-          comments: 'all'
-        }
-      })
-    ]
-  }
-]
+export default {
+  input: 'src/calendar-date-list.js',
+  output: {
+    name: 'calendar-date-list',
+    file: 'dist/calendar-date-list.js',
+    format: 'umd',
+    banner: `/* ${pkg.name} v${pkg.version}, @license MIT */`
+  },
+  plugins: [
+    resolve(),
+    babel({
+      runtimeHelpers: true
+    }),
+    uglify({
+      output: {
+        comments: 'all'
+      }
+    })
+  ]
+}
